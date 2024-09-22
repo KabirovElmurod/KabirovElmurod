28c5dfdbadf90a4304dd82c7f5588c6f99d12f3a
KabirovElmurod <kabirovelmurod5@gmail.com>
2024-09-22 10:26:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('salom')
+
+from pyscript import document
+
+def show(a):
+    show=document.querySelector('#input32')
+    button=document.querySelector('#show')
+
+    if show.type=='password':
+        show.type='text'
+        button.src='show.png'
+    else: 
+        show.type='password'
+        button.src='unshow.png'
